40429afe1be51809a60fa8fea1ed8308027e3f89
Dirision <nick.dirisio@mail.utoronto.ca>
2016-03-27 03:39:39 -0400

    Dual windows

--- GUI/sfmlInject.cpp
+++ GUI/sfmlInject.cpp
@@ -6,21 +6,22 @@ int main()
 {
 	int lambdaChange = 0;
 	//sf::RenderWindow App(sf::VideoMode(640, 480, 32), "SFML Window");
-
-	sf::RenderWindow window(sf::VideoMode(400,400), "SFML");
+	sf::RenderWindow view(sf::VideoMode(1280,720),"Display");
+	sf::RenderWindow window(sf::VideoMode(400,400), "Controls");
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while(window.pollEvent(event)){
 			if (event.type == sf::Event::Closed){window.close();}
 		}
-
+		view.clear();
 		window.clear();/*
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 		{
 			
 
 		}*/
+		view.display();
 		window.display();
 	} // end of while loop
 return 0;
